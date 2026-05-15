31a7e7e191ca2970883f9471f863597fbd7d1e47
udhaya-2 <bhavanaramana8@gmail.com>
2026-05-15 12:11:52 +0530

    remove env file

--- src/firebase/fireBase.js
+++ src/firebase/fireBase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnPL2Xi70sb4lD1jUBUnNLCu_wGFAkW1k",
-  authDomain: "online-code-editor-6fba9.firebaseapp.com",
-  projectId: "online-code-editor-6fba9",
-  storageBucket: "online-code-editor-6fba9.firebasestorage.app",
-  messagingSenderId: "6471303585",
-  appId: "1:6471303585:web:4256bd488d70a5b63e4be0",
-  measurementId: "G-4VLLE4MPRJ"
+  apiKey: import.meta.env.APIKEY,
+  authDomain: import.meta.env.AUTHDOMAIN,
+  projectId: import.meta.env.PROJECTID ,
+  storageBucket: import.meta.env.STORAGEBUCKET,
+  messagingSenderId: import.meta.env.MESSAGINGSENDERID ,
+  appId: import.meta.env.APPID ,
+  measurementId: import.meta.env.MEASUREMENTID
 };
 
 // Initialize Firebase
